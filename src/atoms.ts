070f8965e30a7d53b28966d7ce7330de19d14ef0
Kirill Kuznetsov <kdevlab@yandex.ru>
2026-03-29 13:17:30 +0300

    Add root

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,12 +1,24 @@
 import { atom } from "@kdeveloper/kvark/family";
 
-export const demoAtom = atom({
-  debugLabel: 'Demo',
+export const rootAtom = atom({
+  debugLabel: 'Root',
   async get() {
+    return 'Root atom';
+  },
+});
+
+export const demoAtom = atom({
+  dependencies: { root: rootAtom },
+  debugLabel: 'Demo',
+  async get(ctx) {
+    const rootValue = await ctx.get("root");
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    return 'Hello, world!';
+    return `Hello, world! ${rootValue}`;
   },
   onMount(set) {
-    set('Mounted');
+    const id = setInterval(() => {
+      set(new Date().toISOString());
+    }, 1000);
+    return () => clearInterval(id);
   },
 });
